7d3c66c946399442fcfac31effac29b784344291
sabrineazh <sabrine.azh@gmail.com>
2025-03-09 22:18:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+
+# Charger le modèle
+model = tf.keras.models.load_model("model_cats_dogs.h5")
+
+# Fonction pour faire une prédiction
+def predict_image(img):
+    img = img.resize((150, 150))  
+    img_array = image.img_to_array(img)  
+    img_array = np.expand_dims(img_array, axis=0)  
+    img_array /= 255.0  
+
+    prediction = model.predict(img_array)
+    return "Chien 🐶" if prediction[0][0] > 0.5 else "Chat 🐱"
+
+# Interface Streamlit
+st.title(" Détection de chien 🐶 ou chat 🐱")
+st.write("Chargez une image pour obtenir la prédiction.")
+
+uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image_pil = Image.open(uploaded_file)  # Ouvrir l'image
+    st.image(image_pil, caption="Image chargée", use_column_width=True)
+
+    if st.button("Prédire"):
+        result = predict_image(image_pil)
+        st.success(f"Résultat : {result}")
